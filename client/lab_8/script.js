efe612be04f45946c2b8812a6ba567e6e4474aab
Brice Tikum <btikum18@umd.edu>
2022-11-22 09:18:21 -0500

    Final for Lab 8

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -96,10 +96,18 @@ function initMap() {
 function markerPlace(array, map) {
   console.log('markerPlace', array);
   // const marker = L.marker([51.5, -0.09]).addTo(map);
-  array.forEach((item) => {
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item, index) => {
     const {coordinates} = item.geocoded_column_1;
-    console.log(item);
     L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    if (index === 0) {
+      map.setView([coordinates[1], coordinates[0]], 10);
+    }
   });
 }
 
@@ -155,7 +163,7 @@ async function mainEvent() {
       console.log(event.target.value);
       const newList = filterList(currentList, event.target.value);
       injectHTML(newList);
-      markerPlace(currentList, pageMap);
+      markerPlace(newList, pageMap);
     });
 
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
